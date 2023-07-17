6c01880eb497c185c1063fde8e4858c4d4803adb
Santosh <dsantosh366@gmail.com>
2023-07-17 21:42:34 +0530

    second ; lead model complited with excel file opload

--- backnd/backnd/views_emp.py
+++ backnd/backnd/views_emp.py
@@ -7,11 +7,37 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail,EmailMultiAlternatives
 from django.conf import settings
 from app.models import *
+from django.db.models import Q
 
 # Employee dashboard 
 @login_required
 def EmployeeDashboard(request):
-    data = {
-            'lead_list': LeadModel.objects.all() # lead model object
+    if request.user.is_superuser:
+        return HttpResponse("<h1>Not a valid user to access this page.!</h1>")
+    else:
+        lead = LeadModel.objects.all().order_by('-id') # lead model object
+
+        # post for data searching propose
+        if request.method == 'POST':
+            search = request.POST['search'].strip()  # strip for remove space from both of sides
+
+            # according to input data is filtering 
+            lead = LeadModel.objects.filter(Q(lead_id__icontains = search) | Q(first_name__icontains = search) | Q(email_id__icontains = search) | Q(primary_phone__icontains = search))
+            
+
+        # all data sent on html file in object format 
+        data = {
+            'lead_list': lead
         }
     return render(request,'employee/dashboard.html',data)
+
+
+
+
+
+
+
+
+
+
+
--- backnd/backnd/views_super.py
+++ backnd/backnd/views_super.py
@@ -7,17 +7,30 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail,EmailMultiAlternatives
 from django.conf import settings
 from app.models import *
+from django.db.models import Q
+
 
 # Dashboard or home of a superuser 
 @login_required
 def SuperUserDashBoard(request):
-    if request.user.is_superuser:
+    if not request.user.is_superuser:
+        return HttpResponse("<h1>Not a valid user to access this page.!</h1>")
+    else:
+        lead = LeadModel.objects.all().order_by('-id') # lead model object
+
+        # post for data searching propose
+        if request.method == 'POST':
+            search = request.POST['search'].strip() # strip for remove space from both of sides
+
+            # according to input data is filtering 
+            lead = LeadModel.objects.filter(Q(lead_id__icontains = search) | Q(first_name__icontains = search) | Q(email_id__icontains = search) | Q(primary_phone__icontains = search))
+            
+
+        # all data sent on html file in object format 
         data = {
-            'lead_list': LeadModel.objects.all() # lead model object
+            'lead_list': lead
         }
         return render(request,"super/dashboard.html",data)
-    else:
-        return HttpResponse("<h1>Not a valid user to access this page.!</h1>")
 
 
 
@@ -172,4 +185,6 @@ def userDelete(request,id):
     messages.success(request,"Remove user successfully")
 
     return redirect('superuser_user_list')
+
+
 
