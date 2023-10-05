2589b735624847aa999388223bc350457b8749fb
santoshDasgit <101993217+santoshDasgit@users.noreply.github.com>
2023-10-06 00:40:14 +0530

    Update settings.py

--- backnd/backnd/settings.py
+++ backnd/backnd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ks(xd)45214tc%1_s!ahudlld=-qmxw@9-b38n)8k=&b&44cx3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
