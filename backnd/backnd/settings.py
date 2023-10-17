80ce287c3ff7a2ec9351f839bb7b24e2bb07a6d3
Santosh <dsantosh366@gmail.com>
2023-10-17 18:41:38 +0530

    Revert "database update"
    This reverts commit 44a807eee7f4fe68e3784b3666ddab15980f1749.

--- backnd/backnd/settings.py
+++ backnd/backnd/settings.py
@@ -75,23 +75,11 @@ WSGI_APPLICATION = 'backnd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.mysql',
-      'NAME': "ezyhelpers",
-      'USER':'root',
-      'PASSWORD':'Ezyhelpers@2023',
-      'HOST':'localhost',
-      'PORT':'3306',
-      'TIME_ZONE': 'Asia/Kolkata',
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
